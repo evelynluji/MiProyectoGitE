e216f0d5ed1e90a10734f5afbd2c8f8367f95f65
Evelyn <lucianoe620@gmail.com>
2020-05-16 17:39:22 -0500

    crea archivos iniciales del proyecto

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,"core/home.html") 
+
+def about(request):
+    return render(request,"core/about.html") 
+
+    
+def porfolio(request):
+    return render(request,"core/porfolio.html") 
+
+
+def contact(request):
+    return render(request,"core/contact.html") 
